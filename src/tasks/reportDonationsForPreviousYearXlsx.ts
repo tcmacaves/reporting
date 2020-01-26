3c3578883923ae45bc0b34a7755c84150f3daf24
Andy Edwards <jedwards@fastmail.com>
2020-01-25 23:10:51 -0600

    feat: add annual-donations script

---
+++ src/tasks/reportDonationsForPreviousYearXlsx.ts
@@ -0,0 +1,23 @@
+import reportDonationsXlsx from '../reports/reportDonationsXlsx'
+import getYearDateRange from '../util/getYearDateRange'
+
+async function go(): Promise<void> {
+  const lastYear = new Date()
+  lastYear.setFullYear(
+    process.argv[2] ? parseInt(process.argv[2]) : lastYear.getFullYear() - 1,
+    0,
+    1
+  )
+  const [startDate, endDate] = getYearDateRange(lastYear.getFullYear())
+
+  await reportDonationsXlsx({
+    file: `TCMA Donations ${startDate.getFullYear()}.xlsx`,
+    startDate,
+    endDate,
+  })
+}
+
+go().catch(error => {
+  console.error(error.stack) // eslint-disable-line no-console
+  process.exit(1)
+})
